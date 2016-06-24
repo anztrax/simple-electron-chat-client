ccbb10de0dec25a307bebe8a3966db40b3723b2a
andrew ananta <kuro.ketanitem@gmail.com>
2016-06-25 00:16:19 +0700

    initial commit

---
+++ Views/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Main extends React.Component{
+  render(){
+    return (
+      <div>
+        <h3>Hello from React and electron with es2015 :)</h3>
+        
+      </div>
+    )
+  }
+}
+
+export default Main;
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from '../Views/main.jsx';
+
+(function(){
+  ReactDOM.render(<Main />, document.body);
+})();
